d94f83f6b4707bb07ff7fc2e58108afeda9c18f1
James Perih <jperih@gasbuddy.com>
2018-03-22 09:55:57 -0600

    Infrastructure to support a List One item.

--- src/routes/web/list.js
+++ src/routes/web/list.js
@@ -15,6 +15,7 @@ module.exports.listAll = (req, res) => {
       let dinos = [];
       resp.hits.hits.forEach((hit) => {
         dinos.push({
+          id: hit._id,
           series: hit._source.DinoSeries,
           name: hit._source.DinoName,
           date: hit._source.DinoDate,
@@ -24,6 +25,32 @@ module.exports.listAll = (req, res) => {
       });
 
       res.render('list-index', { dinos: dinos.sort((a, b) => b.date.localeCompare(a.date)) }, (err, html) => {
+        res.send(html);
+      });
+    }
+  });
+}
+
+module.exports.listOne = (req, res) => {
+  const dinoId = req.params.id;
+  esClient.get({
+    index: 'dinos',
+    id: dinoId,
+    type: '_all',
+  }, (err, resp) => {
+    let dino = {};
+
+    if (!err) {
+      dino = {
+        id: resp.id,
+        series: resp._source.DinoSeries,
+        name: resp._source.DinoName,
+        date: resp._source.DinoDate,
+        desc: resp._source.DinoDescription,
+        imgURL: resp._source.DinoImageURL,
+      };
+
+      res.render('list-one', { dino }, (err, html) => {
         res.send(html);
       });
     }
